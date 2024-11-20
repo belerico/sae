Fixed normalization

sae/__main__.py
@@ -2,15 +2,16 @@ import os
 from contextlib import nullcontext, redirect_stdout
 from dataclasses import dataclass
 from multiprocessing import cpu_count
-from safetensors.torch import load_model
 
 import torch
 import torch.distributed as dist
 from datasets import Dataset, load_dataset
+from safetensors.torch import load_model
 from simple_parsing import field, parse
-from transformers import AutoModel, AutoTokenizer, BitsAndBytesConfig, PreTrainedModel
+from transformers import (AutoModel, AutoTokenizer, BitsAndBytesConfig,
+                          PreTrainedModel)
 
-from .data import chunk_and_tokenize, MemmapDataset
+from .data import MemmapDataset, chunk_and_tokenize
 from .trainer import SaeTrainer, TrainConfig
 
 

sae/config.py
@@ -115,8 +115,11 @@ class TrainConfig(Serializable):
     save_every: int = 1000
     """Save SAEs every `save_every` steps."""
 
-    normalize_activations: str | None = "expected_norm_1"
-    """Normalize activations. Cjoose between `expected_norm_1` or `norm_1`"""
+    normalize_activations: float | None = None
+    """Normalize the activations to have an expected norm of `normalize_activations`."""
+
+    num_norm_estimation_tokens: int = 1_000_000
+    """Number of tokens to use for estimating the normalization factor."""
 
     hook: Callable[
         [nn.Module, Tuple[Any, ...], Any, Dict[nn.Module, str], Dict[str, Tensor]],
@@ -132,11 +135,11 @@ class TrainConfig(Serializable):
         assert not (
             self.layers and self.layer_stride != 1
         ), "Cannot specify both `layers` and `layer_stride`."
-        if (
-            self.normalize_activations is not None
-            and self.normalize_activations.lower() not in {"expected_norm_1", "norm_1"}
-        ):
-            raise ValueError(
-                "`normalize_activations` must be one of `expected_norm_1` or `norm_1`. "
-                "To disable activations normalization pass None instead."
-            )
+        # if (
+        #     self.normalize_activations is not None
+        #     and self.normalize_activations.lower() not in {"expected_norm_1", "norm_1"}
+        # ):
+        #     raise ValueError(
+        #         "`normalize_activations` must be one of `expected_norm_1` or `norm_1`. "
+        #         "To disable activations normalization pass None instead."
+        #     )

sae/trainer.py
@@ -1,7 +1,7 @@
+import copy
 import os
 from collections import defaultdict
 from dataclasses import asdict
-import copy
 from fnmatch import fnmatchcase
 from functools import partial
 from typing import cast
@@ -17,12 +17,11 @@ from tqdm.auto import tqdm
 from transformers import PreTrainedModel
 
 from .config import TrainConfig
+from .normalization import estimate_norm_scaling_factor
 from .sae import Sae
 from .utils import (
     CycleIterator,
-    ExpectedNorm1Normalizer,
     L1Scheduler,
-    Norm1Normalizer,
     geometric_median,
     get_layer_list,
     get_lr_scheduler,
@@ -172,15 +171,25 @@ class SaeTrainer:
 
         # Normalize activations
         if cfg.normalize_activations:
-            act_normalizer = (
-                ExpectedNorm1Normalizer()
-                if "expected" in cfg.normalize_activations
-                else Norm1Normalizer()
-            )
-            act_normalizer.to(self.model.device)
-            self.act_normalizers = {
-                hook: copy.deepcopy(act_normalizer) for hook in self.local_hookpoints()
+            name_to_module = {
+                name: self.model.get_submodule(name) for name in self.cfg.hookpoints
             }
+            module_to_name = {v: k for k, v in name_to_module.items()}
+            scaling_factors = estimate_norm_scaling_factor(
+                dl,
+                model,
+                cfg.num_norm_estimation_tokens,
+                cfg.hook,
+                module_to_name=module_to_name,
+                target_norm=cfg.normalize_activations,
+                device=device,
+            )
+            if dist.is_available() and dist.is_initialized():
+                scaling_factors = {
+                    name: dist.all_reduce(scaling_factor, op=dist.ReduceOp.AVG)
+                    for name, scaling_factor in scaling_factors.items()
+                }
+            self.scaling_factors = scaling_factors
 
     def load_state(self, path: str):
         """Load the trainer state from disk."""
@@ -210,14 +219,6 @@ class SaeTrainer:
                 f"{path}/l1_scheduler.pt", map_location=device, weights_only=True
             )
             self.l1_scheduler.load_state_dict(l1_state)
-        if self.cfg.normalize_activations:
-            for name, act_normalizer in self.act_normalizers.items():
-                act_norm_state = torch.load(
-                    f"{path}/{name}_act_normalizer.pt",
-                    map_location=device,
-                    weights_only=True,
-                )
-                act_normalizer.load_state_dict(act_norm_state)
 
         for name, sae in self.saes.items():
             load_model(sae, f"{path}/{name}/sae.safetensors", device=str(device))
@@ -272,8 +273,12 @@ class SaeTrainer:
         avg_fvu = defaultdict(float)
         avg_auxk_loss = defaultdict(float)
         avg_multi_topk_fvu = defaultdict(float)
-        running_l2_act_norm = 1.0
-        avg_act_norm = 0.0
+        avg_act_norm = defaultdict(float)
+        running_mean_act_norm = {}
+
+        # Function to update the running mean
+        def update_running_mean(running_mean, new_value, count):
+            return (running_mean * (count - 1) + new_value) / count
 
         hidden_dict: dict[str, Tensor] = {}
         name_to_module = {
@@ -297,7 +302,7 @@ class SaeTrainer:
                     partial(
                         self.cfg.hook,
                         module_to_name=module_to_name,
-                        hidden_dict=hidden_dict
+                        hidden_dict=hidden_dict,
                     )
                 )
                 for mod in name_to_module.values()
@@ -342,18 +347,26 @@ class SaeTrainer:
                 if raw.cfg.normalize_decoder:
                     raw.set_decoder_norm_to_unit_norm()
 
+                # Normalize the activations
+                if self.cfg.normalize_activations:
+                    with torch.no_grad():
+                        hiddens = hiddens * self.scaling_factors[name]
+
+                # Save the running mean of the L2 norm of the activations
+                l2_norm = hiddens.norm(p=2, dim=-1).mean()
+                if name not in running_mean_act_norm:
+                    running_mean_act_norm[name] = l2_norm
+                else:
+                    running_mean_act_norm[name] = update_running_mean(
+                        running_mean_act_norm[name], l2_norm, batch_idx + 1
+                    )
+
                 acc_steps = self.cfg.grad_acc_steps * self.cfg.micro_acc_steps
                 denom = acc_steps * self.cfg.wandb_log_frequency
                 wrapped = maybe_wrapped[name]
 
                 # Save memory by chunking the activations
                 for chunk in hiddens.chunk(self.cfg.micro_acc_steps):
-                    if self.cfg.normalize_activations:
-                        chunk = self.act_normalizers[name](chunk)
-                    avg_act_norm += chunk.norm(p=2, dim=-1).mean().item() / denom
-                    running_l2_act_norm += (
-                        chunk.norm(p=2, dim=-1).mean().item() - running_l2_act_norm
-                    ) / (batch_idx + 1)
                     out = wrapped(
                         chunk,
                         dead_mask=(
@@ -396,7 +409,9 @@ class SaeTrainer:
                     if self.cfg.sae.k <= 0:
                         current_l1_coefficient = 0.0
                     else:
-                        current_l1_coefficient = self.l1_scheduler.current_l1_coefficient
+                        current_l1_coefficient = (
+                            self.l1_scheduler.current_l1_coefficient
+                        )
 
                     loss = (
                         recon_loss
@@ -479,8 +494,14 @@ class SaeTrainer:
 
                     info.update(
                         {
-                            "norm/avg_act_norm": avg_act_norm,
-                            "norm/running_l2": running_l2_act_norm,
+                            f"norm/avg_act_norm_{name}": avg_act_norm[name]
+                            for name in self.saes
+                        }
+                    )
+                    info.update(
+                        {
+                            f"norm/running_mean_act_norm_{name}": running_mean_act_norm[name]
+                            for name in self.saes
                         }
                     )
 
@@ -490,7 +511,7 @@ class SaeTrainer:
                     avg_l0.clear()
                     avg_l2.clear()
                     avg_multi_topk_fvu.clear()
-                    avg_act_norm = 0.0
+                    avg_act_norm.clear()
 
                     if self.cfg.distribute_modules:
                         outputs = [{} for _ in range(dist.get_world_size())]
@@ -616,10 +637,7 @@ class SaeTrainer:
             if self.cfg.sae.k <= 0:
                 torch.save(self.l1_scheduler.state_dict(), f"{path}/l1_scheduler.pt")
             if self.cfg.normalize_activations:
-                for name, act_normalizer in self.act_normalizers.items():
-                    torch.save(
-                        act_normalizer.state_dict(), f"{path}/{name}_act_normalizer.pt"
-                    )
+                torch.save(self.scaling_factors, f"{path}/scaling_factors.pt")
 
             self.cfg.save_json(f"{path}/config.json")
 

train.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     )
     model = AutoModel.from_pretrained(
         model_name,
-        device_map={"": "mps"},
+        device_map={"": "cuda"},
         torch_dtype=torch.float32,
         trust_remote_code=True,
     )
@@ -37,18 +37,20 @@ if __name__ == "__main__":
         ),
         batch_size=batch_size,
         save_every=25_000,
-        layers=list(range(6)),
+        layers=[3],
         lr=1e-3,
         lr_scheduler_name="constant",
         lr_warmup_steps=0.0005,
         lr_decay_steps=0.0,
-        l1_coefficient=1.0,
+        l1_coefficient=3,
         l1_warmup_steps=0.005,
         max_seq_len=max_seq_len,
         use_l2_loss=True,
         cycle_iterator=True,
         num_training_tokens=1_000_000_000,
-        normalize_activations="expected_norm_1",
+        normalize_activations=1,
+        num_norm_estimation_tokens=8_000_000,
+        run_name="pythia-70m-deduped-1024-lambda-0.5-lr-7e-4",
     )
     trainer = SaeTrainer(cfg, data_loader, model)
     trainer.fit()