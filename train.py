f3e4d283bc5f7e96c1f10c9d3813504a9167411a
belerico <belo.fede@outlook.com>
2024-12-31 10:19:47 +0000

    Fix train.py

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
+import os
+
 import torch
+import torch.distributed as dist
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 from transformers import AutoModel, AutoTokenizer
@@ -7,7 +10,17 @@ from sae import SaeConfig, SaeTrainer, TrainConfig
 from sae.data import chunk_and_tokenize_streaming
 
 if __name__ == "__main__":
-    model_name = "EleutherAI/pythia-160m-deduped"
+    # Init DDP
+    local_rank = os.environ.get("LOCAL_RANK")
+    ddp = local_rank is not None
+    rank = int(local_rank) if ddp else 0
+    if ddp:
+        torch.cuda.set_device(int(local_rank))
+        dist.init_process_group("nccl")
+        print(f"Rank {rank}, Using DDP across {dist.get_world_size()} GPUs.")
+        print(f"Rank {rank}, Local device: {torch.cuda.current_device()}")
+
+    model_name = "EleutherAI/pythia-70m-deduped"
     l1_coefficient = 0.5
     max_seq_len = 512
     target_l0 = 64
@@ -15,8 +28,7 @@ if __name__ == "__main__":
     lr = 7e-4
 
     dataset = load_dataset(
-        "allenai/c4",
-        "en",
+        "togethercomputer/RedPajama-Data-1T-Sample",
         split="train",
         trust_remote_code=True,
         streaming=True,
@@ -24,40 +36,26 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.pad_token = tokenizer.eos_token
     dataset = chunk_and_tokenize_streaming(dataset, tokenizer, max_seq_len=max_seq_len)
-    # dataset = load_dataset(
-    #     "NeelNanda/pile-small-tokenized-2b",
-    #     streaming=True,
-    #     split="train",
-    #     trust_remote_code=True,
-    # )
+    dataloader = DataLoader(dataset, batch_size=batch_size)
 
-    def from_tokens(x):
-        return {
-            "input_ids": torch.stack(list(torch.tensor(example["tokens"]) for example in x), dim=0)
-        }
-
-    dataloader = DataLoader(
-        dataset,
-        # collate_fn=from_tokens,
-        batch_size=batch_size,
-    )
     model = AutoModel.from_pretrained(
         model_name,
         device_map={"": "cuda"},
         torch_dtype=torch.float32,
         trust_remote_code=True,
     )
+    sae_cfg = SaeConfig(
+        expansion_factor=16,
+        k=16,
+        jumprelu=False,
+        jumprelu_target_l0=target_l0,
+        init_enc_as_dec_transpose=True,
+        multi_topk=True,
+    )
     cfg = TrainConfig(
-        SaeConfig(
-            expansion_factor=16,
-            k=-1,
-            jumprelu=True,
-            jumprelu_target_l0=target_l0,
-            init_enc_as_dec_transpose=True,
-        ),
+        sae=sae_cfg,
         batch_size=batch_size,
-        save_every=25_000,
-        layers=[3],
+        save_every=10000,
         lr=lr,
         lr_scheduler_name="cosine",
         lr_warmup_steps=0.01,
@@ -65,15 +63,20 @@ if __name__ == "__main__":
         l1_warmup_steps=0.1,
         max_seq_len=max_seq_len,
         use_l2_loss=True,
-        cycle_iterator=True,
         num_training_tokens=1_000_000_000,
         normalize_activations=1,
-        num_norm_estimation_tokens=2_000_000,
-        run_name="checkpoints/pythia-160m-deduped-1024-lambda-{}-target-L0-{}-lr-{}".format(
-            l1_coefficient, target_l0, lr
+        num_norm_estimation_tokens=1_000_000,
+        run_name="checkpoints/{}-1024-lambda-{}-target-L0-{}-lr-{}".format(
+            model_name,
+            l1_coefficient,
+            target_l0,
+            lr,
         ),
         adam_betas=(0.0, 0.999),
         adam_epsilon=1e-8,
+        distribute_modules=True,
+        keep_last_n_checkpoints=2,
+        auxk_alpha=1 / 32,
     )
     trainer = SaeTrainer(cfg, dataloader, model)
     trainer.fit()
